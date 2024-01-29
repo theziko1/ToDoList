69ca7d9bb0162cf619af529237a5c28dcc938d72
soka1998 <soukainaharifi18@gmail.com>
2024-01-29 16:36:42 +0000

    Create Task Container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { LuListTodo } from "react-icons/lu";
+import Table from "../components/Table";
 
 const Home = () => {
   return (
     <>
-      <div className="bg-gradient-to-r from-blue-500 to-purple-700 h-screen flex justify-center items-center">
+      <div className="bg-gradient-to-r from-blue-500 to-purple-700 m-0 p-0 h-screen flex justify-center items-center">
         <section className="bg-white w-[70%] h-[70%] rounded-2xl">
           <div className="flex justify-start m-4 gap-4 text-xl">
             <div className="text-3xl">
@@ -12,6 +13,7 @@ const Home = () => {
             </div>
             <h1 className="font-['Inter'] italic text-3xl">Task Lists</h1>
           </div>
+          <Table />
         </section>
       </div>
     </>
