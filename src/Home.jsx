c06a13df70b1f6ee39e2664774d3bb1721b996c2
soka1998 <soukainaharifi18@gmail.com>
2024-01-29 14:27:31 +0000

    stage

---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className="container max-sm:600px  mx-auto  py-0">
+              <h1 className='my-0 text-align: center;'>Task Lists</h1>
+        <div className="top">
+            <input type="text" placeholder='add task..' />
+            <div className="add">Add</div>
+
+
+        </div>
+
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
