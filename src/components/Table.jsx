f128e7647f2fd142a317c757e257c266d5e2a008
theziko1 <zakariaghachim96@gmail.com>
2024-01-30 14:56:59 +0100

    Update Table component and home page

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -10,7 +10,7 @@ const Table = () => {
     
  const TimeLoading = setTimeout(() => {
     setIsLoading(!isLoading)
- },4000)
+ },2000)
  
  return () => clearTimeout(TimeLoading);
   }, [])
@@ -23,10 +23,13 @@ const Table = () => {
       <table className="table-auto w-full gap-6 flex flex-col items-center justify-center bg-white font-['Inter']">
         
           <tr className="w-full flex justify-around">
-            <th className="py-2 px-4 border-b h-10">ID</th>
-            <th className="py-2 px-4 border-b h-10">Task</th>
-            <th className="py-2 px-4 border-b h-10">Priority</th>
+            <th className="py-2 px-4 border-b h-10">Title</th>
+            <th className="py-2 px-4 border-b h-10">Priorité</th>
+            <th className="py-2 px-4 border-b h-10">Status</th>
+            <th className="py-2 px-4 border-b h-10">Description</th>
             <th className="py-2 px-4 border-b h-10">Actions</th>
+            <th className="py-2 px-4 border-b h-10">Créer par</th>
+            <th className="py-2 px-4 border-b h-10">Deadline</th>
           </tr>
        
         
@@ -35,15 +38,19 @@ const Table = () => {
             <Skeleton />
          
         ) : (
-              <tr className=" w-full flex justify-around">
-              <td className="py-2 px-4 border-b h-10">Ligne 1, Colonne 1</td>
-                <td className="py-2 px-4 border-b h-10">Ligne 1, Colonne 2</td>
-                <td className="py-2 px-4 border-b h-10">Ligne 1, Colonne 3</td>
+              <tr className="w-full flex justify-around">
+                <td className="py-2 px-4 border-b h-10">Task 1 </td>
+                <td className="py-2 px-4 border-b h-10">Ligne 1</td>
+                <td className="py-2 px-4 border-b h-10">Ligne 1</td>
+                <td className="py-2 px-4 border-b h-10">Ligne 1</td>
                 <td className="py-2 px-4 border-b h-10">
                       <div className="flex flex-row gap-10 justify-center">
                       <FaCheck color="green"/><IoTrashBin color="red"/>
                       </div>
                 </td>
+              
+                <td className="py-2 px-4 border-b h-10">Zakaria</td>
+                <td className="py-2 px-4 border-b h-10">Ligne 1</td>
           </tr>
           
           
