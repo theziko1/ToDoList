e886023e1d986a5a6e0a098c0f4ea3a671eb17c8
theziko1 <zakariaghachim96@gmail.com>
2024-01-31 15:10:45 +0100

    login and logout methods

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,5 +1,9 @@
 import { User } from "../models/Schema";
 import { bcrypt } from "bcrypt";
+import { config } from "dotenv";
+import jwt from "jsonwebtoken"
+
+config()
 
 const signUp = async (req, res) => {
   try {
@@ -14,4 +18,45 @@ const signUp = async (req, res) => {
   }
 };
 
-module.exports = signUp;
+// Login Method
+
+export const login = async (req  , res ) => {
+  try { 
+     
+     const { email , password} = req.body
+     const User = await User.findOne({ email })  
+ 
+     if(!User) {
+       return res.status(401).json({error : "Authentication failed"})
+     }
+     const MatchedPass = bcrypt.compare(password,User.password)
+     if (!MatchedPass) {
+        return res.status(401).json({error : "Authentication failed"})
+     }
+     const token = jwt.sign({ userId: User._id }, process.env.SECRET_KEY, {
+        expiresIn: '6h',
+        });
+     res.cookie('token',token)   
+     res.status(200).json({token}) 
+     
+  } catch (error) {
+     res.status(500).json({error : "login failed"})
+  }
+}
+
+
+
+
+// Logout Method
+
+  export const logout = (req ,res ) => {
+     try {
+        res.clearCookie('token')
+        res.status(200).json({success : true, message : "logged out"})
+     } catch (error) {
+        res.status(500).json({success : false, message : "something went wrong"})
+     }
+  }
+
+
+module.exports = {signUp , login , logout};
